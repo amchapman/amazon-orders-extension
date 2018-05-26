0646f7b27ece60db227b949bf1a7726f49aa9125
Alex Chapman <amchapman@users.noreply.github.com>
2018-05-26 11:30:23 -0400

    simplify url matching and fix item/price handling

--- background.js
+++ background.js
@@ -25,18 +25,11 @@ function downloadURI(uri, name) {
 
 function downloadOrders(orders) {
   downloadURI(
-  	'data:text/csv;charset=utf-8;base64,' + window.btoa(orders), 
+  	'data:text/csv;charset=utf-8;base64,' + window.btoa(orders),
   	"amazon-orders.csv"
   );
 }
 
-var urlRegex = /^https?:\/\/(?:[^\.]+\.)?amazon\.com/;
-
 chrome.pageAction.onClicked.addListener(function(tab) {
-  if (urlRegex.test(tab.url)) {
-    chrome.tabs.sendMessage(tab.id, { text: "send_orders" }, downloadOrders);
-  }
+  chrome.tabs.sendMessage(tab.id, { text: "send_orders" }, downloadOrders);
 });
-
-
-
--- content.js
+++ content.js
@@ -2,27 +2,21 @@ chrome.runtime.onMessage.addListener(function(msg, sender, sendResponse) {
   if (msg.text && (msg.text == "send_orders")) {
     var x = document.getElementsByClassName("order");
     var csv = '"Order Placed","Total","Ship To","Order #","Items",\n';
-    var vals, itemViews, prices;
-    for (i = 0; i < x.length; i++) { 
-      vals = x[i].querySelectorAll(".value");
-      itemViews = x[i].querySelectorAll(".item-view-left-col-inner");
-      prices = x[i].querySelectorAll(".a-color-price");
-
-      for (j = 0; j < vals.length; j++) { 
-        csv += '"' + vals[j].innerText + '",';
-      }
-
-      // Item titles
+    for (var i = 0; i < x.length; i++) {
+      // Order Places, Total, Ship To, Order #
+      var vals = x[i].querySelectorAll(".value");
+      for (var j = 0; j < vals.length; j++) csv += '"' + vals[j].innerText + '",';
       csv += '"';
-      for (j = 0; j < itemViews.length; j++) { 
-        if (j>0) csv += "/";
-        csv += itemViews[j].childNodes[1].childNodes[1].title.replace('"','""');
-        if (j<prices.length) csv += " (" + prices[j].innerText + ")";
-      }
 
-      // New Line
+      // Items
+      var items = x[i].querySelectorAll(".a-link-normal");
+      var prices = x[i].querySelectorAll(".a-color-price");
+      for (var j=0; j < prices.length; j++) {
+        if (j) csv += "/";
+        csv += items[(j*2)+3].innerText.replace('"','""') + " (" + prices[j].innerText + ")";
+      }
       csv += '"\n';
-  }
+    }
     sendResponse(csv);
   }
 });
